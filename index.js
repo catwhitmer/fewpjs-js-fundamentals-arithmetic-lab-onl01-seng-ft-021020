e33912b76705b5dced7b72d3d6fa749c462d0d01
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-23 12:41:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+
+let random = Math.ceil(Math.random()) ;
+
+
+let mod = 19 % 5;
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 10, 15, 20);
