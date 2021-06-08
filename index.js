eaf8c719bf82cc089e5784755efd8055090f574a
Cat Whitmer <catwhitmer@gmail.com>
2021-06-08 18:38:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ let num2 = 31;
 let multiply = num1 * num2;
 
 
+<<<<<<< HEAD
 let random = Math.ceil(Math.random()) ;
 
 
 let mod = 19 % 5;
 
 let max = Math.max(1, 2, 3, 4, 5, 6, 10, 15, 20);
+=======
+
+let random = Math.floor(Math.random() * 10) +> 1;
+>>>>>>> 6bcf5136e6230e282175374a83b3857493111eac
